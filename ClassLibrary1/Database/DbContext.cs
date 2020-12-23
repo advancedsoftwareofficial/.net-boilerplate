3aeed0f1bc90af0b6197640deefabb5499659f9d
Amr Swalha <amr@advancedsoftware.io>
2020-12-23 17:15:00 +0400

    adding Advanced Software Packages

---
+++ ClassLibrary1/Database/DbContext.cs
@@ -0,0 +1,23 @@
+﻿using AdvancedSoftware.DataAccess.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace NETBoilerplate.DataAccess.Database
+{
+    public class DbContext : AppDbContext<DbContext>
+    {
+        public DbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+        }
+    }
+}
